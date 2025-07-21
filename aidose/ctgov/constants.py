a8651f76860a9274ff6526619d892136071fc593
sssohrab <sohrab.ferdowsi@gmail.com>
2025-07-21 10:56:07 +0200

    Makedirs for CTGOV_DATASET_PATH
    If ran for the first time.

--- aidose/ctgov/constants.py
+++ aidose/ctgov/constants.py
@@ -3,6 +3,8 @@ from aidose import CORPORA_ROOT_DIR, RESOURCES_DIR
 import os
 
 CTGOV_DATASET_PATH = os.path.join(CORPORA_ROOT_DIR, "CTGOV")
+os.makedirs(CTGOV_DATASET_PATH, exist_ok=True)
+
 CTGOV_DATASET_RAW_PATH = os.path.join(CTGOV_DATASET_PATH, "raw")
 
 CTGOV_API_DOWNLOAD_BASE_URL = "https://clinicaltrials.gov/api/v2"
