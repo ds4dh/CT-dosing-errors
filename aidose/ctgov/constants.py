ad404d4e61a4b5559b1f3389b36cf689938ef2a1
sssohrab <sohrab.ferdowsi@gmail.com>
2025-07-22 14:00:10 +0200

    Rename resource to upper-case

--- aidose/ctgov/constants.py
+++ aidose/ctgov/constants.py
@@ -9,4 +9,4 @@ CTGOV_DATASET_RAW_PATH = os.path.join(CTGOV_DATASET_PATH, "raw")
 
 CTGOV_API_DOWNLOAD_BASE_URL = "https://clinicaltrials.gov/api/v2"
 
-CTGOV_NCTIDS_LIST_ALL_PATH = os.path.join(RESOURCES_DIR, "ctgov", "ctgov_nctids_list_all.txt")
+CTGOV_NCTIDS_LIST_ALL_PATH = os.path.join(RESOURCES_DIR, "CTGOV", "ctgov_nctids_list_all.txt")
