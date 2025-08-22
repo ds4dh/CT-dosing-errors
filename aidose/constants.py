855ea3bf42cd0cf8f6ba3fda5d5e5af962025a37
sssohrab <sohrab.ferdowsi@gmail.com>
2025-08-22 15:45:22 +0200

    Tweak dataset root management passed from env-var

--- aidose/constants.py
+++ aidose/constants.py
@@ -5,3 +5,5 @@ REPO_DIR = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
 RESOURCES_DIR = os.path.join(REPO_DIR, "resources")
 
 DATASETS_ROOT = os.environ.get("DATASETS_ROOT", None)
+if DATASETS_ROOT is not None:
+    os.makedirs(DATASETS_ROOT, exist_ok=True)
